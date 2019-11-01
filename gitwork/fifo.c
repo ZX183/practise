4f0709c893076ba74008bb9e8618205db534bade
TS_Han_Jue <lx19080617@partner.sharp.co.jp>
2019-11-01 15:15:21 +0800

    new fifo.c

--- gitwork/fifo.c
+++ gitwork/fifo.c
@@ -1,23 +1,18 @@
 /*====================================================
 *   Copyright (C) 2019  All rights reserved.
 *
-*   文件名称：file.c
+*   文件名称：fifo.c
 *   创 建 者：zx  2658159372@qq.com
 *   创建日期：2019年11月01日
 *   描    述：
 ================================================================*/
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-	FILE *fp = fopen(argv[1],"w+");
-	if(fp == NULL)
-		perror("fopen");
-	fputs("hell",fp);
-	fputs("world!\n",fp);
-	fputs("\n",fp);
-	fclose(fp);
+	int fd[2] = {0};
+	
     return 0;
 }
